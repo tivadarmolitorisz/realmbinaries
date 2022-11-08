b7e76022f6f82814f5fff77fa9e1fca35658c937
Tivadar Molitorisz <tivadar.molitorisz@supercharge.io>
2022-11-08 16:00:44 +0100

    chore: update realm 10.32.2

--- Package.swift
+++ Package.swift
@@ -16,13 +16,13 @@ let package = Package(
   targets: [
     .binaryTarget(
             name: "Realm",
-            url: "https://github.com/tivadarmolitorisz/realmbinaries/releases/download/10.29.0/Realm.xcframework.zip",
-            checksum: "a3eddd60659e50a947702cc2875cab005c8f608b776ff70124e9276c5169e28f"
+            url: "https://github.com/tivadarmolitorisz/realmbinaries/releases/download/10.32.2/Realm.xcframework.zip",
+            checksum: "ea2aa1fc26c49e387deae1719cabb02b944c4acfd446f912a31a0475334874b1"
         ),
     .binaryTarget(
             name: "RealmSwift",
-            url: "https://github.com/tivadarmolitorisz/realmbinaries/releases/download/10.29.0/RealmSwift.xcframework.zip",
-            checksum: "8231dd3e25fae4b0ab71aed2b3d09efef39e5b9022266deddba7f2ea78d4329b"
+            url: "https://github.com/tivadarmolitorisz/realmbinaries/releases/download/10.32.2/RealmSwift.xcframework.zip",
+            checksum: "be9326bec7a7e962a1fb91cd5f7dc9eb2b7112b3380b797b037f56947bb866a4"
         ),
   ],
   swiftLanguageVersions: [.v5]
